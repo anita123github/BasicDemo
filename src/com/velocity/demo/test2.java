35ab7afc767a0f1a64cc83cc7103b77e043b190f
anita <anubhui27@gmail.com>
2023-01-18 22:00:22 +0530

    add class

---
+++ src/com/velocity/demo/test2.java
@@ -0,0 +1,16 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer>al=new ArrayList<Integer>();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		System.out.println(al);
+
+	}
+
+}
